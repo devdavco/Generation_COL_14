77e72bf2c3e3e0a700f0f7a898968cfa540fb7bc
devdavco <github.ferocity308@passmail.com>
2026-09-15 17:38:50 -0500

    ejercicio 8 done

--- 3_Nivel_Dificil/ejercicio8.js
+++ 3_Nivel_Dificil/ejercicio8.js
@@ -6,3 +6,13 @@ let perfil = {
     ciudad: "Medellín",
 
 };
+
+console.log(`Perfil completo: ${perfil}`)
+console
+console.log(perfil.nombre)
+console.log(perfil['apellido'])
+perfil['edad'] = 29
+//console.log(perfil.edad)
+perfil.hobby = "fotografía";
+delete perfil.ciudad
+console.log(perfil)
